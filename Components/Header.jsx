944a759450659d243a0818249c987035432dab7f
neo1415 <adedaniel502@gmail.com>
2023-03-04 11:56:52 +0100

    fix

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -3,7 +3,6 @@ import {getCategories} from '../services'
 import Link from 'next/link'
 import Image from 'next/image'
 import { assets } from '../Constants'
-import { BsTwitter, BsFacebook, BsInstagram, BsLinkedin } from "react-icons/bs";
 
 const Header = () => {
 
